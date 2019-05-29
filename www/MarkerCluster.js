02bcef868931b3ee075c66fa084bdd58b145f5d3
fsingstad <31435957+fsingstad@users.noreply.github.com>
2019-05-29 15:48:11 +0200

    Update MarkerCluster.js

--- www/MarkerCluster.js
+++ www/MarkerCluster.js
@@ -514,7 +514,7 @@ Object.defineProperty(MarkerCluster.prototype, '_redraw', {
     var nwCell = geomodel.getGeocell(expandedRegion.northeast.lat, expandedRegion.southwest.lng, cellLen);
     var seCell = geomodel.getGeocell(expandedRegion.southwest.lat, expandedRegion.northeast.lng, cellLen);
 
-    if (currentZoomLevel > self.maxZoomLevel || resolution === 0) {
+    if (currentZoomLevel > 21 || resolution === 0) {
       console.log('currentZoomLevel: '+currentZoomLevel+', maxZoomLevel: '+self.maxZoomLevel+', resolution: '+resolution);
       resolution = self.OUT_OF_RESOLUTION;
     }
